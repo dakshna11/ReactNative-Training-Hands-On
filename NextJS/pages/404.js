2b1503eff5e48ba8688646563f4ef3a32ec33ff5
dakshna11 <54468002+dakshna11@users.noreply.github.com>
2021-12-16 20:42:33 +0530

    Add files via upload

---
+++ NextJS/pages/404.js
@@ -0,0 +1,5 @@
+function PageNotFound(){
+    return <h2>404 Page with all styling</h2>
+}
+
+export default PageNotFound
--- NextJS/pages/index.js
+++ NextJS/pages/index.js
@@ -1,5 +1,21 @@
+import Link from "next/link"
+import {useRouter} from 'next/router'
+
 function Home(){
-  return <h1>Home Page</h1>
+  const router = useRouter()
+  const handleClick  = ()=>{
+    console.log('Placing order')
+    router.replace('/product')
+  }
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href='/blog'><a>Blog</a></Link>
+      <Link href='/product'><a>Products</a></Link>
+      <Link href='/posts'><a>Posts</a></Link>
+      <button onClick={handleClick}>Place Order</button>
+    </div>
+  )
 }
 
 export default Home
