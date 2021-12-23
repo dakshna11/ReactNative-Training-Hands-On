7be1a00fc6b763073572d016512e9d0064712fbb
dakshna11 <54468002+dakshna11@users.noreply.github.com>
2021-12-23 18:27:06 +0530

    Add files via upload

---
+++ Spotify-Web-App/atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+    key:"playlistIdState",
+    default: "71WNtKfZ89PmvkRhDgDbdQ"
+})
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null
+})
